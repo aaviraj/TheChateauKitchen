0226a97e7482a4d96deaca10b11f815608a106c5
aaviraj <aviraj.bhattacharya2020@vitbhopal.ac.in>
2023-04-29 22:27:04 +0530

    fifth

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
-import './Header.css';
+import { images } from '../../constants';
+import { SubHeading } from '../../components';
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app__wrapper section__padding" id="home" >
+    <div className="app__wrapper_info">
+      <SubHeading/>
+
+    </div>
+
+    <div className='app__wrapper_img'>
+
+    </div>
   </div>
 );
 
