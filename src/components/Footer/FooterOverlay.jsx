8b5b1fd4a727d6889d1a5e1a55617cef9eedff04
aaviraj <aviraj.bhattacharya2020@vitbhopal.ac.in>
2023-05-03 02:02:15 +0530

    Final commit

--- src/components/Footer/FooterOverlay.jsx
+++ src/components/Footer/FooterOverlay.jsx
@@ -3,8 +3,11 @@ import React from 'react';
 import './FooterOverlay.css';
 
 const FooterOverlay = () => (
-  <div>
-    FooterOverlay
+  <div className='app_footerOverlay'>
+    <div className='app__footerOverlay-black' />
+    <div className='app__footerOverlay-img app__bg' />
+
+    
   </div>
 );
 
--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
 import './Newsletter.css';
-
+import SubHeading from '../SubHeading/SubHeading';
 const Newsletter = () => (
-  <div>
+  <div className='app__newsletter'>
+    <div className='app__newsletter-heading'>
+      <SubHeading title='newsletter' />
+      <h1 className='headtext__cormorant'>Subscribe To Our Newsletter</h1>
+      <p className='p__opensans'>And never miss latest updates!</p>
+    </div>
+    <div className='app__newsletter-input flex__center'>
+      <input type="email" placeholder='Enter your e-mail address' />
+      <button className='custom__button'>Subscribe</button>
+
+    </div>
     Newsletter
   </div>
 );
